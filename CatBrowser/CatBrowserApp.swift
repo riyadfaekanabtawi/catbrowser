4fdea38f48ea559d46a30644fac2889a8046d8fb
Riyad Anabtawi <riyad.faek@gmail.com>
2025-02-03 10:59:22 -0700

    Added ios project

---
+++ CatBrowser/CatBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatBrowserApp.swift
+//  CatBrowser
+//
+//  Created by Riyad Anabtawi on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CatBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatBrowser
+//
+//  Created by Riyad Anabtawi on 03/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
